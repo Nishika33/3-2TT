79b4bb5e373a08e1610f0f27db116045f84ce481
Nishika-33 <gandhamnishika@gmail.com>
2026-02-26 11:39:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Reg from './Reg'
+import Home from'./Home'
+
+function App() {
+  return (
+    <>
+    <h1>I am App</h1>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/reg" element={<Reg/>}/> 
+      <Route path="/h" element={<Home/>}/>
+     </Routes>
+    </BrowserRouter></>
+  );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+function Home(){
+    return(
+        <>
+        <h1>I am Home page</h1>
+        </>
+    )    
+}
+export default Home;
